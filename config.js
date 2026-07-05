af80763148200dadd0b7aa642d5aae1168d2be40
antsev487 <antony.sevastelis@gmail.com>
2026-07-05 13:44:24 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // Create a free Supabase project, then paste your values below.
 // Supabase > Project Settings > API
 
-const SUPABASE_URL = "https://qiegyokxmdbuviezubbp.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://qiegyokxmdbuviezubbp.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFpZWd5b2t4bWRidXZpZXp1YmJwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMyMzMwOTcsImV4cCI6MjA5ODgwOTA5N30.IHw9RHxMdDhZZ-xJq-F7s_2N34pBX_k3l6PT25gBR-I";
